be0c303f9ea340307f5bdcf38f500a9de42e2230
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-22 18:34:39 +0200

    Load Comments link

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { useParams, Route } from 'react-router-dom';
+import { useParams, Route, Link } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import { Fragment } from 'react';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
@@ -25,6 +25,12 @@ const QuoteDetails = () => {
         text={quote.text} 
         author={quote.author}
       />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className='centered'>
+          <Link to={`/quotes/${params.quoteId}/comments`} className='btn--flat'>Load Comments</Link>
+        </div>
+      </Route>
+
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
