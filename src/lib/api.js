61e87e95bb2a967539384b31cdb5b2c331d76ae0
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-24 17:16:57 +0200

    Sending & Getting Quote Data via Http

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://react-prep-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://react-redux-e4c1d-default-rtdb.firebaseio.com';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,16 +1,48 @@
 import QuoteList from '../components/quotes/QuoteList';
+import useHttp from '../hooks/use-http';
+// import { useHistory } from 'react-router-dom';
+import LoadingSpinner  from '../components/UI/LoadingSpinner';
+import { useEffect } from 'react';
+import { getAllQuotes } from '../lib/api';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Hennadii', text: 'a Learning React'},
-  { id: 'q2', author: 'Zhukov', text: 'c Learning Angular'},
-  { id: 'q3', author: 'Arigos', text: 'y Learning Vue is not good'},
-];
+// const DUMMY_QUOTES = [
+//   { id: 'q1', author: 'Hennadii', text: 'a Learning React'},
+//   { id: 'q2', author: 'Zhukov', text: 'c Learning Angular'},
+//   { id: 'q3', author: 'Arigos', text: 'y Learning Vue is not good'},
+// ];
 
 const AllQuotes = () => {
+  const { sendRequest, status, data: loadedQuotes, error } = useHttp(getAllQuotes, true);
+//   const history = useHistory();
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div>
+        <LoadingSpinner /> 
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
   return (
     <>
-      <QuoteList 
+      {/* <QuoteList 
         quotes={DUMMY_QUOTES}
+      /> */}
+      <QuoteList 
+        quotes={loadedQuotes}
       />
     </>
   );
